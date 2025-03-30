84eca0e44f94aeea1c3c03f685776f457bbffe6e
jpgilchrist <james.p.gilchrist@gmail.com>
2025-03-30 18:50:38 -0400

    CH5 Website L1 Copy Static

--- src/main.py
+++ src/main.py
@@ -1,9 +1,31 @@
 from textnode import TextNode, TextType
+import os
+import shutil
+
+
+def copy_all(src, dest):
+    if not os.path.exists(src):
+        raise Exception("Source directory does not exist", src)
+
+    if not os.path.exists(dest):
+        os.mkdir(dest)
+
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dest, item)
+        if os.path.isdir(s):
+            copy_all(s, d)
+        else:
+            shutil.copy(s, d)
+
+
+def build():
+    shutil.rmtree("public", ignore_errors=True)
+    copy_all("static", "public")
 
 
 def main():
-    text_node = TextNode("this is some text", TextType.LINK, "https://boot.dev")
-    print(text_node)
+    build()
 
 
 if __name__ == "__main__":
